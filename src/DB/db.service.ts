c4c09389a973f00c353f9fe5be6f23031fb49f2e
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-02-04 13:45:10 +0600

    feat: complete /album route

--- src/DB/db.service.ts
+++ src/DB/db.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { AlbumEntity } from 'src/albums/entities/albumEntity';
 import { ArtistEntity } from 'src/artists/entities/artistEntity';
 import { TrackEntity } from 'src/tracks/entities/trackEntity';
 import { UserEntity } from 'src/users/entities/userEntity';
@@ -8,4 +9,5 @@ export class DBService {
   users: UserEntity[] = [];
   tracks: TrackEntity[] = [];
   artists: ArtistEntity[] = [];
+  albums: AlbumEntity[] = [];
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AlbumModule } from './albums/album.module';
 import { ArtistModule } from './artists/artist.module';
 import { Database } from './DB/db.module';
 import { TrackModule } from './tracks/track.module';
 import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [Database, UserModule, TrackModule, ArtistModule],
+  imports: [Database, UserModule, TrackModule, ArtistModule, AlbumModule],
 })
 export class AppModule {}
