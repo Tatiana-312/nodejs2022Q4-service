6d8c99644f9a7d757f4cdb6cf65359d72f856a64
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-02-04 21:55:22 +0600

    feat: add ability to get PORT from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import * as dotenv from 'dotenv';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,6 @@ async function bootstrap() {
     }),
   );
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
