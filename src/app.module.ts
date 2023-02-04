7e7d6b15e83283736ab990dee99bca0d908dd576
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-02-04 12:49:08 +0600

    feat: complete artist.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ArtistModule } from './artists/artist.module';
 import { Database } from './DB/db.module';
 import { TrackModule } from './tracks/track.module';
 import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [Database, UserModule, TrackModule],
+  imports: [Database, UserModule, TrackModule, ArtistModule],
 })
 export class AppModule {}
---
+++ src/artists/artist.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Post, Put } from "@nestjs/common";
+import { ArtistService } from "./artist.service";
+import { CreateArtistDto } from "./Dtos/createArtist.dto";
+import { UpdateArtistDto } from "./Dtos/updateArtist.dto";
+import { ArtistEntity } from "./entities/artistEntity";
+
+@Controller('track')
+export class ArtistController {
+  constructor(private readonly artistService: ArtistService) {}
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistService.create(createArtistDto);
+  }
+
+  @Get()
+  async findAll(): Promise<ArtistEntity[]> {
+    return this.artistService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.artistService.findOne(id);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ) {
+    return this.artistService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.artistService.remove(id);
+  }
+}
