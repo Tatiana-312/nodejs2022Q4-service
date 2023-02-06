ecaaac089ffa9b7fb948cc8f940c0811be21e2e7
Tatiana-312 <tatyanapetrosh@gmail.com>
2023-02-06 22:33:22 +0600

    feat: add info to readme file and refactor api.yaml

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
